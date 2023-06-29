29038ada4d12603198dd3d350dc288ff104b7a97
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-30 00:38:04 +1000

    Updated reactions model

--- models/reaction.js
+++ models/reaction.js
@@ -1,31 +1,34 @@
 const mongoose = require('mongoose');
 
-const reactionSchema = new mongoose.Schema({
-    reactionId: {
-        type: mongoose.Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
+const { Schema } = mongoose;
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toISOString(),
+        },
     },
-    reactionBody: {
-        type: String,
-        required: 'Reaction is Required',
-        minlength: 1,
-        maxlength: 280
-    },
-    username: {
-        type: String,
-        required: 'Username is Required'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        // Use a getter method to format the timestamp on query
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-},{
-    toJSON: {
-        getters: true
+    {
+        toJSON: {
+            getters: true,
+        },
     }
-});
+);
 
 const Reaction = mongoose.model('Reaction', reactionSchema);
 
