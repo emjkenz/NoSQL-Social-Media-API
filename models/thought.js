71ffa159868c47778a94b88493c0723f171f45d2
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-30 00:37:53 +1000

    Updated thoughts model

--- models/thought.js
+++ models/thought.js
@@ -2,28 +2,38 @@ const mongoose = require('mongoose');
 
 const { reactionSchema } = require('./reaction');
 
-const thoughtSchema = new mongoose.Schema({
-    thoughtText: {
-        type: String,
-        required: 'Thought is Required',
-        minlength: 1,
-        maxlength: 280
+const { Schema } = mongoose;
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toISOString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
     },
-    username: {
-        type: String,
-        required: 'Username is Required'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        // Use a getter method to format the timestamp on query
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-    reactions: [reactionSchema],
-    reactionCount: {
-        type: Number,
-        default: 0
-    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
